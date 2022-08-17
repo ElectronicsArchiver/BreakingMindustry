a11d97de90a03f5d1c4f910d748a284f4d7966d0
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-17 14:17:47 +0800

    moar bullets let's goooo

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -3,12 +3,13 @@ let block = 8
 Blocks.duo.size = 2
 Blocks.duo.reload = 20
 Blocks.duo.range = block*17
+Blocks.duo.category = Category.production // ah yes, production turret
 Blocks.duo.requirements = ItemStack.with(Items.copper, 55, Items.lead, 30)
 Blocks.duo.ammo(
 	Items.copper,
 	extend(BasicBulletType, {
-		width: 10,
-		height: 10,
+		width: 9,
+		height: 9,
 		speed: 7,
 		damage: 20,
 		lifetime: 30
@@ -21,5 +22,25 @@ Blocks.duo.ammo(
 		damage: 22,
 		reloadMultiplier: 2.5,
 		lifetime: 30
-	})
+	}),
+	Items.graphite,
+	extend(BasicBulletType, {
+		width: 9,
+		height: 9,
+		speed: 6,
+		damage: 28,
+		reloadMultiplier: 0.6,
+		lifetime: 37.5
+	}),
+	Items.silicon,
+	extend(BasicBulletType, {
+		width: 9,
+		height: 9,
+		speed: 9,
+		damage: 25,
+		reloadMultiplier: 1.35,
+		lifetime: 30,
+		homingRange = block*17,
+		homingPower = 0.6
+	}),
 )
