6c288638f628068a09dfcd4caf516ae3a44b5d77
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-17 10:07:29 +0800

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,8 +1,13 @@
-const a = extend(Router, "a", {
+// region test
+const test = extend(Router, "test", {
 	requirements: ItemStack.with(Items.copper, 1, Items.lead, 3),
+	buildVisibility: BuildVisibility.shown,
+	category: Category.turret,
 	size: 2,
 	localizedName: "bejir"
 	health: 1000
 }):
-
-exports.a = a
+// end test
+module.exports = {
+	test = test
+};
