45db8807dad340e692a4bcd3e73bd84d4566f96f
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-17 13:42:45 +0800

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -9,13 +9,32 @@ const test = extendContent(Wall, "test", {
 
 const test2 = extendContent(Wall, "test2", {});
 test2.health = 200;
-buildVisibility = BuildVisibility.shown;
+test2.buildVisibility = BuildVisibility.shown;
 test2.requirements = ItemStack.with(
 	Items.copper, 1000
 );
 test2.category = Category.defense;
 
+const test3 = extend(Wall, "test3", {
+	health: 200,
+	buildVisibility: BuildVisibility.shown,
+	requirements: ItemStack.with(
+		Items.copper, 1000
+	),
+	category: Category.defense
+});
+
+const test4 = extendContent(Wall, "test4", {});
+test4.health = 200;
+test4.buildVisibility = BuildVisibility.shown;
+test4.requirements = ItemStack.with(
+	Items.copper, 1000
+);
+test2.category = Category.defense;
+
 module.exports = {
-	test = test,
-	test2 = test2
+	test: test,
+	test2: test2,
+	test3: test3,
+	test4: test4
 }
