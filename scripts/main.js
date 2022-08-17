3cf31a8fcd2fb349fded7508ce719c68221f16d7
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-17 10:32:03 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-require("blocks");
+print('Loading spinner...')
+
+const SpinBlock = extend(Block, {
+    update: function(tile){
+        tile.rotation(tile.rotation() + 1)
+    }
+})
+const spinner = new SpinBlock("spinner")
+spinner.update = true
+spinner.localizedName = "Spinner"
+spinner.description = "it spin"
+spinner.rotate = true
+spinner.category = Category.turret
+spinner.buildVisibility = BuildVisibility.shown
+
+print("Created spinner block.") 
