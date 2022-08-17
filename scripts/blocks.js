0afad2965044d8130cdacc7bf6c64e754bea5e74
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-17 13:58:14 +0800

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,3 +1,21 @@
 Blocks.duo.size = 3
 Blocks.duo.reload = 5
-Blocks.duo.name = "what"
+Blocks.duo.ammo(
+	Items.copper,
+	extend(BasicBulletType, {
+		width: 10,
+		height: 10,
+		speed: 7,
+		damage: 20,
+		lifetime: 9 * Blocks.duo.range
+	}),
+	Items.lead,
+	extend(BasicBulletType, {
+		width: 6,
+		height: 7,
+		speed: 9,
+		damage: 22,
+		reloadMultiplier: 2.5,
+		lifetime: 9 * Blocks.duo.range
+	})
+)
