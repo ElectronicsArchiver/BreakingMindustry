239b3077be5fd3a0fe93dd44ce57aa3557a07660
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-18 19:07:49 +0800

    wtf

--- scripts/blocks/list.js
+++ scripts/blocks/list.js
@@ -1,7 +1,17 @@
 // turret
-const turrets = 
+let counter = 0;
+const names = 
 	["duo", "scatter", "scorch", "hail", "wave", 
 	"lancer", "arc", "parallax", "swarmer",
 	"salvo", "segment", "tsunami", "fuse",
 	"ripple", "cyclone", "foreshadow", "spectre",
-	"meltdown"]
+	"meltdown"];
+
+l();
+l();
+l();
+
+function l(){
+	require("turrets/" + names[counter]);
+	counter++;
+}
