a5746870a39951ecc93a305b60412db0434368c9
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-18 20:33:47 +0800

    Delete menu.js

--- scripts/menu.js
+++
@@ -1,6 +0,0 @@
-Events.on(ClientLoadEvent, event => {
-	const dialog = new BaseDialog("Hello.");
-	dialog.cont.add("Are you sure you want to break the game?");
-	dialog.button.buttons("Yes", dialog.hide()).size(100, 50);
-	dialog.button.buttons("No", Core.app.exit()).size(100, 50);
-});
