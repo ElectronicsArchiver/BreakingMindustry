533b6f812a8b8ba4d95337f08921879765d9d8ad
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-19 21:06:51 +0800

    Update list.js

--- scripts/blocks/list.js
+++ scripts/blocks/list.js
@@ -1,17 +1,13 @@
-
 (() => {
-    
     const turrets = [
-    	'duo' ,	'scatter' , 'scorch' ,
-    	// 'hail' , 'wave' , 'lancer' ,
+	'duo' ,	'scatter' , 'scorch' ,
+	'hail' //, 'wave' , 'lancer' ,
     	// 'arc' , 'parallax' , 'swarmer' ,
     	// 'salvo' , 'segment' , 'tsunami' ,
     	// 'fuse' , 'ripple' , 'cyclone' ,
     	// 'foreshadow' , 'spectre' , 'meltdown'
     ]
 
-
     for(let turret of turrets)
         require('blocks/turrets/' + turret);
-
 })();
