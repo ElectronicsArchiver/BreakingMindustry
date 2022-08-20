78fd65d0f0a3519a94b6929328a5a5fb31f8bf83
Drifted Notes <109656172+driftednotes@users.noreply.github.com>
2022-08-20 14:25:50 +0800

    Update Hail.js

--- scripts/blocks/turrets/Hail.js
+++ scripts/blocks/turrets/Hail.js
@@ -11,20 +11,63 @@ module.exports = {
     specs : {
         targetGround : true ,
         inaccuracy : 5 ,
-        reload : 12.5 ,
-        range : block * 28 ,
+        reload : 75.35 ,
+        range : block * 35 ,
         size : 4
     },
         
-    recipe : [ Items.copper , 1 ],
+    recipe : [ 
+        Items.copper , 234,
+        Items.lead , 85,
+        Items.graphite, 76
+    ],
     
     ammo : [{
-        material : Items.copper ,
-        lifetime : 80 ,
-        height : 8.5 ,
-        damage : 12 ,
-        width : 8.5 ,
-        speed : 8 ,
-        type : 'basic'
-    }]
+        material : Items.graphite ,
+        type : 'artillery',
+        speed : 5, 
+        damage : 20,
+        knockback : 0.8,
+        lifetime : 80,
+        width : 11,
+        height: 11,
+        collidesTiles : false,
+        splashDamageRadius: 25 * 0.85,
+        splashDamage: 72
+    },{
+        material : Items.silicon ,
+        type : 'artillery',
+        speed : 3, 
+        damage : 20,
+        knockback : 0.8,
+        lifetime : 80,
+        width : 11,
+        height : 11,
+        collidesTiles : false,
+        splashDamageRadius : 25 * 0.85,
+        splashDamage : 80,
+        reloadMultiplier : 1.2,
+        ammoMultiplier : 3,
+        homingPower : 0.04,
+        homingRange : 50
+    },{
+        material : Items.pyratite ,
+        type : 'artillery',
+        speed : 5, 
+        damage : 25,
+        knockback : 0.8,
+        lifetime : 80,
+        width : 13,
+        height: 13,
+        collidesTiles : false,
+        splashDamageRadius: 25 * 0.85,
+        splashDamage: 76,
+        status : StatusEffects.burning,
+        statusDuration : 60 * 8,
+        frontColor : Pal.lightishOrange,
+        backColor : Pal.lightOrange,
+        makeFire : true,
+        trailEffect : Fx.incendTrail,
+        ammoMultiplier : 4
+    }]*
 }
